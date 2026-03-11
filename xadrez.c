574e41e5a2632994cf344ed9f107503a749ffb15
amarquesengsoft <amarques4912@gmail.com>
2026-03-11 01:25:13 +0000

    Criação de variáveis das peças e estruturas de repetição para movimentação das peças.

--- xadrez.c
+++ xadrez.c
@@ -8,14 +8,44 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+        int torre = 5, bispo = 5, rainha = 8;
+        int i;
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+        printf("Movimento do Bispo:\n");
+
+        i = 1;
+        while (i <= bispo)
+        {
+            printf("Cimma/Direita\n");
+            i++;
+        }
+        
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+        printf("Movimento da Torre:\n");
+
+        for (i = 1; i <= torre; i++)
+        {
+            printf("Direita\n");
+        }
+        
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+        printf("Movimento da Rainha\n");
+
+        i = 1;
+        do
+        {
+            printf("Esquerda\n");
+            i++;
+        } while (i <= rainha);
+        
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
