177390eda2f0c80067e76fe563176050f81cc0c5
amarquesengsoft <amarques4912@gmail.com>
2026-03-17 00:05:01 +0000

    desafio nível mestre implementação de movimentos com recursividade e loop for complexo

--- xadrez.c
+++ xadrez.c
@@ -4,84 +4,83 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+//Código de recursividade para movimentação da torre:
+void moverTorre(int casas){
+    if(casas > 0){
+        printf("Direita\n");
+        moverTorre(casas -1);
+    }
+}
+
+//Código de recursividade para movimentação do bispo
+void moverBispo(int casasBispo){
+    if(casasBispo > 0){
+        printf("Direita\n");
+        printf("Cima\n");
+        moverBispo(casasBispo - 1);
+    }
+}
+
+//Código de recursividade para movimentação da rainha
+void moverRainha(int casasRainha){
+    if(casasRainha > 0){
+        printf("Esquerda\n");
+        moverRainha(casasRainha - 1);
+    }
+}
+
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Declaração das variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-        int torre = 5, bispo = 5, rainha = 8;
-        int i;
-
+    // Nível Mestre - Movimentação das Peças
     // Implementação de Movimentação do Bispo
-    // Utilização de estrutura de repetição "while" para simular a movimentação do Bispo em diagonal.
+    // Utilização de estrutura de repetição com loop aninhado "for" para simular a movimentação do Bispo em diagonal.
 
-        printf("Movimento do Bispo:\n");
+        printf("Movimento do Bispo com Loop aninhado:\n");
 
-        i = 1;
-        while (i <= bispo)
+        int casasBispo2 = 5;
+        for (int cima = 1; cima <= casasBispo2; cima++)
         {
-            printf("Cimma/Direita\n");
-            i++;
+            for ( int direita = 1; direita <= 1; direita++){
+                printf("Direita\n");
+            }
+            printf("Cima\n");    
         }
+        
+
+    // Implementação de recursividade para simulação do movimento do Bispo em diagonal:
+
+        printf("Movimentação do Bispo com Recursividade:\n");
+
+        moverBispo(5);
         
     // Implementação de Movimentação da Torre
-    // Utilização de estrutura de repetição "for" para simular a movimentação da Torre para a direita.
+    // Implementação de recursividade para simulação do movimento da Torre para a direita.
 
-        printf("Movimento da Torre:\n");
+        printf("Movimento da Torre com recursividade:\n");
 
-        for (i = 1; i <= torre; i++)
-        {
-            printf("Direita\n");
-        }
-        
+        moverTorre(5);
 
     // Implementação de Movimentação da Rainha
-    // Utilize de estrutura de repetição "do while" para simular a movimentação da Rainha para a esquerda.
+    // Implementação de recursividade para simulação do movimento da Rainha para a esquerda.
 
-        printf("Movimento da Rainha\n");
+        printf("Movimento da Rainha com recursividade:\n");
 
-        i = 1;
-        do
-        {
-            printf("Esquerda\n");
-            i++;
-        } while (i <= rainha);
-        
+        moverRainha(8);
 
-    //Realização do Desafio Nível Aventureiro
-    //Implementação do código para movimentação do Cavalo com loop for
+    // Implementação de Movimentação do Cavalo
+    // Utilização de estrutura de repetição complexa "for" para simular a movimentação do Cavalo, 
+    //sendo duas casas para cima e uma casa para direita .
 
-        printf("Movimento do Cavalo Com Loop For\n");
+    printf("Movimento do Cavalo com Loop aninhado:\n");
 
+        for (int cima = 1, direita = 1; cima <= 2 && direita <= 1; cima++) {
 
-        for(int movimentoCavalo = 1; movimentoCavalo <= 1; movimentoCavalo++)
-        {
-            for (int movimentoCavalo2 = 1; movimentoCavalo2 <= 2; movimentoCavalo2++)
-            {
-                printf("Cima com For\n");
-            }
-            printf("Lado com For\n");
+        printf("Cima\n");
+
+        if (cima == 2) {
+            printf("Direita\n");
+            direita++;
         }
-
-    //Implementação do código para movimentação do cavalo como loop while
-
-        
-        printf("Movimento do Cavalo Com Loop While\n");
-
-        int movimentoCavaloWhile = 1;
-        int movimentoCavaloWhile2 = 1;
-
-        while (movimentoCavaloWhile <= 1)
-        {
-            while (movimentoCavaloWhile2 <= 2)
-            {
-                printf("Cima com While\n");
-                movimentoCavaloWhile2++;
-            }
-                printf("Lado com While\n");
-                movimentoCavaloWhile++;
-        }
-        
-        
+    }
 
     return 0;
 }
